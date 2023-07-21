328df1c38c20fddf1a9061d845065923b61de84f
FormApi <siddeswarapradeep@gmail.com>
2023-07-21 16:51:13 +0530

    update

---
+++ Model/form_model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema
+
+const formSchema = new schema({
+    name: {
+        type: String,
+        required : true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase:true,
+    },
+    mobile: {
+        type: Number,
+        required:true
+    },
+    message: {
+        type: String,
+    }
+
+    
+})
+
+const formDB = mongoose.model("form", formSchema)
+module.exports = formDB
